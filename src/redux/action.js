357c3ed62c45d19ea9d1d1c0fdc149fba23a3cbe
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-11-13 13:47:34 +0700

    fix action bugs

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,118 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, ToastAndroid, Image} from 'react-native';
-import {
-  ScrollView,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native-gesture-handler';
-import {addShop, getMyShop} from '../../../controller/Shop';
-import {styles} from '../../../styles/styles';
-import Button from '../../../components/Button';
-import ImagePicker from 'react-native-image-picker';
-import {useDispatch, useSelector} from 'react-redux';
-import {setShopId} from '../../../redux/action';
-
-const ShopDashboard = ({navigation}) => {
-  const [shop, setShop] = useState(null);
-  const [shopName, setShopName] = useState('');
-  const [photo, setPhoto] = useState(null);
-  const [description, setDescription] = useState('');
-  const [loading, setLoading] = useState(false);
-
-  //redux
-  const {token, user} = useSelector((state) => state);
-  const shopReducer = useSelector((state) => state.shop);
-  const dispatch = useDispatch();
-  //
-  const updateShop = () => {
-    setLoading(true);
-    addShop(shopName, description, photo, user.id, token)
-      .then((res) => {
-        ToastAndroid.show(res.message, ToastAndroid.LONG);
-        setLoading(false);
-        navigation.goBack();
-      })
-      .catch((err) => {
-        ToastAndroid.show(`${err}`, ToastAndroid.LONG);
-        setLoading(false);
-      });
+const changeToken = (data) => {
+  return {
+    type: 'CHANGE',
+    data: data,
   };
-
-  const handleChoosePhoto = () => {
-    const options = {
-      noData: true,
-    };
-    ImagePicker.launchImageLibrary(options, (response) => {
-      if (response.uri) {
-        setPhoto(response);
-      }
-    });
-  };
-
-  const getShop = () => {
-    setLoading(true);
-    getMyShop(user.id, token)
-      .then((res) => {
-        if (res.data) {
-          setShop(res.data);
-          setShopName(res.data.shop_name);
-          dispatch(setShopId(res.data.id));
-          setDescription(res.data.description);
-        }
-        setLoading(false);
-      })
-      .catch((err) => {
-        ToastAndroid.show(`${err}`, ToastAndroid.LONG);
-        setLoading(false);
-      });
-  };
-
-  useEffect(() => {
-    getShop();
-  }, []);
-
-  return (
-    <ScrollView contentContainerStyle={[styles.container, styles.screen]}>
-      <Text style={styles.textTitle}>Edit Nama Toko</Text>
-      <View style={styles.marginVerticalMini}>
-        <Text>Nama Toko</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Edit Nama Toko"
-          onChangeText={(inputShopName) => setShopName(inputShopName)}
-          value={shopName}
-        />
-      </View>
-      <View style={styles.marginVerticalMini}>
-        <Text>Deskripsi Toko</Text>
-        <TextInput
-          style={[styles.textInput, styles.textInputMultiline]}
-          placeholder="Edit Deskripsi"
-          onChangeText={(inputDescription) => setDescription(inputDescription)}
-          value={description}
-          multiline={true}
-        />
-      </View>
-      <TouchableOpacity
-        style={styles.centerContainer}
-        onPress={() => handleChoosePhoto()}>
-        <Image
-          style={styles.profileImageLarge}
-          source={
-            photo
-              ? photo
-              : shop
-              ? {
-                  uri:
-                    'http://tokomu.herokuapp.com/uploads/shops/' + shop.image,
-                }
-              : require('../../../assets/img/user-shape.png')
-          }
-        />
-      </TouchableOpacity>
-      <Button title="Simpan" onPress={() => updateShop()} isLoading={loading} />
-    </ScrollView>
-  );
 };
 
-export default ShopDashboard;
+const clearToken = () => {
+  return {
+    type: 'CLEAR',
+  };
+};
+
+const setUser = (id, email, name, role) => {
+  return {
+    type: 'SET_USER',
+    data: {
+      id: id,
+      email: email,
+      name: name,
+      role: role,
+    },
+  };
+};
+
+const setShopId = (data) => {
+  return {
+    type: 'SET_ID',
+    data: data,
+  };
+};
+export {changeToken, clearToken, setUser, setShopId};
